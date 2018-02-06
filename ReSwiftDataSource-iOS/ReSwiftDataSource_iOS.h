a24120bd665f642b7442aa1347eb0c1349ac3b99
SiSo Mollov <mollov@me.com>
2018-02-06 13:11:10 +0200

    add Carthage support

---
+++ ReSwiftDataSource-iOS/ReSwiftDataSource_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ReSwiftDataSource_iOS.h
+//  ReSwiftDataSource-iOS
+//
+//  Created by SiSo Mollov on 6.02.18.
+//  Copyright © 2018 SiSo Mollov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReSwiftDataSource_iOS.
+FOUNDATION_EXPORT double ReSwiftDataSource_iOSVersionNumber;
+
+//! Project version string for ReSwiftDataSource_iOS.
+FOUNDATION_EXPORT const unsigned char ReSwiftDataSource_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSwiftDataSource_iOS/PublicHeader.h>
+
+
